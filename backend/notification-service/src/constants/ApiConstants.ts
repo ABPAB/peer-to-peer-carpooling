276e9ba224e73fa21b42ea8beb7c8a720475730f
Pragati Upadhyay <pragati29122@gmail.com>
2024-12-04 17:41:29 +0530

    refactored the notification-service

---
+++ backend/notification-service/src/constants/ApiConstants.ts
@@ -0,0 +1,27 @@
+export default {
+  // API constants
+  SCHEME: "https",
+  HOSTNAME: "carpooling.com",
+  VERSION: "v1",
+
+  // API URI
+  RIDE_SERVICE_API_V1: "/trip-service/v1",
+  AUTH_SERVER: "/api/v1/auth",
+
+  // API error code
+  INTERNAL_SERVER_ERROR: "INTERNAL_SERVER_ERROR",
+  BAD_REQUEST: "BAD_REQUEST",
+  UNAUTHORIZED: "UNAUTHORIZED",
+  INVALID_DATA: "INVALID_DATA",
+  RECORD_NOT_FOUND: "RECORD_NOT_FOUND",
+  SERVICE_UNAVAILABLE: "SERVICE_UNAVAILABLE",
+
+  // API error message
+  MESSAGE_INTERNAL_SERVER_ERROR: "Internal Server Error",
+  MESSAGE_BAD_REQUEST: "Bad Request",
+  MESSAGE_NOT_FOUND: "Not Found",
+  MESSAGE_SUCCESS: "Success",
+
+  // API error description
+  DESCRIPTION_INTERNAL_SERVER_ERROR: "Something went wrong!",
+};
